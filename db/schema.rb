787e9ce9914ef3681754582531aa829bb0c42dc3
Paul Yarabe <paulyarabe@post.harvard.edu>
2016-03-03 16:36:40 +0000

    Can delete todo item

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303155659) do
+ActiveRecord::Schema.define(version: 20160303160856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "todo_items", force: true do |t|
+    t.string   "content"
+    t.integer  "todo_list_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id", using: :btree
 
   create_table "todo_lists", force: true do |t|
     t.string   "title"
